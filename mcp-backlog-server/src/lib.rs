d2dd1877a6a787595fe15fec672ef5ad3a347cc9
Yuji Matsumoto <matsumoto@nulab.com>
2025-05-29 23:01:29 +0900

    refactor: Move utility functions to a new `util.rs` file and update references

--- mcp-backlog-server/src/lib.rs
+++ mcp-backlog-server/src/lib.rs
@@ -1,5 +1,6 @@
 mod error;
 mod server;
 pub mod tools;
+mod util;
 
 pub use server::Server;
--- mcp-backlog-server/src/tools/issue.rs
+++ mcp-backlog-server/src/tools/issue.rs
@@ -4,12 +4,12 @@ use backlog_issue::{
     Issue, Milestone,
     requests::{GetIssueListParamsBuilder, UpdateIssueParamsBuilder},
 };
-use std::cmp::Ordering;
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::error::{Error as McpError, Result};
+use crate::util::{MatchResult, find_by_name_from_array};
 
 pub async fn get_issue_details(
     client: Arc<Mutex<BacklogApiClient>>,
@@ -83,59 +83,6 @@ pub async fn get_issues_by_milestone_name_impl(
             original_name: milestone_name_user_input,
             suggestions: None,
         }),
-    }
-}
-
-enum MatchResult<T> {
-    Exact(T),
-    Suggestion(Vec<String>),
-    None,
-}
-
-fn find_by_name_from_array<T: Clone>(
-    array: &[T],
-    name: &str,
-    name_getter: impl Fn(&T) -> &String,
-) -> MatchResult<T> {
-    let preprocessed_name = |name: &str| name.to_lowercase().replace([' ', '　'], "");
-
-    let name = preprocessed_name(name);
-
-    for m in array {
-        if preprocessed_name(name_getter(m)) == name {
-            return MatchResult::Exact(m.clone());
-        }
-    }
-
-    let get_prefix_length = |s1: &str, s2: &str| {
-        s1.chars()
-            .zip(s2.chars())
-            .take_while(|(c1, c2)| c1 == c2)
-            .count()
-    };
-
-    let mut candidates = Vec::new();
-    for m in array {
-        let dist = strsim::levenshtein(&name, &preprocessed_name(name_getter(m)));
-        if dist <= 2 {
-            let prefix_len = get_prefix_length(&name, name_getter(m));
-            candidates.push((name_getter(m), dist, prefix_len));
-        }
-    }
-
-    if candidates.is_empty() {
-        MatchResult::None
-    } else {
-        candidates.sort_by(|a, b| match a.1.cmp(&b.1) {
-            Ordering::Equal => b.2.cmp(&a.2),
-            other => other,
-        });
-        MatchResult::Suggestion(
-            candidates
-                .into_iter()
-                .map(|(name, _, _)| name.clone())
-                .collect(),
-        )
     }
 }
 
