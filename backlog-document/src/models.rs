6afd5dd10ab9cd8b9e091ba2fd9a0bbd6fbf174e
Yuji Matsumoto <matsumoto@nulab.com>
2025-05-30 20:16:40 +0900

    refactor: Replace UserId with User in Document and DocumentDetail structs for improved clarity

--- backlog-document/src/models.rs
+++ backlog-document/src/models.rs
@@ -1,13 +1,13 @@
 use backlog_core::{
-    DocumentId,
-    identifier::{ProjectId, StatusId, UserId},
+    DocumentId, User,
+    identifier::{ProjectId, StatusId},
 };
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-//#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct Document {
     pub id: DocumentId,
     pub project_id: ProjectId,
@@ -16,16 +16,16 @@ pub struct Document {
     pub status_id: i32, // Assuming status_id is an integer, adjust if it's a different type
     #[serde(skip_serializing_if = "Option::is_none")]
     pub emoji: Option<String>,
-    pub created_user_id: UserId,
+    pub created_user: User,
     pub created: DateTime<Utc>,
-    pub updated_user_id: UserId,
+    pub updated_user: User,
     pub updated: DateTime<Utc>,
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub tags: Vec<DocumentTag>, // Or Vec<Tag> if a Tag struct is defined
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-//#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct DocumentDetail {
     pub id: DocumentId,
     pub project_id: ProjectId,
@@ -35,9 +35,9 @@ pub struct DocumentDetail {
     pub status_id: i32, // Assuming status_id is an integer, adjust if it's a different type
     #[serde(skip_serializing_if = "Option::is_none")]
     pub emoji: Option<String>,
-    pub created_user_id: UserId,
+    pub created_user: User,
     pub created: DateTime<Utc>,
-    pub updated_user_id: UserId,
+    pub updated_user: User,
     pub updated: DateTime<Utc>,
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub tags: Vec<DocumentTag>,
@@ -50,7 +50,7 @@ pub struct DocumentTag {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-//#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct DocumentTreeResponse {
     pub project_id: ProjectId,
     pub active_tree: DocumentTreeNode,
@@ -58,7 +58,7 @@ pub struct DocumentTreeResponse {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-//#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase")]
 pub struct DocumentTreeNode {
     pub id: DocumentId,
     #[serde(skip_serializing_if = "Option::is_none")]
